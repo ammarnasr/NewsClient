69bfd958720ee867bd0c9d8efc8382e5e7105bff
ammarnasr <ammarnasraza@gmail.com>
2020-12-02 14:25:28 +0200

    add id column as primariy key

--- db.go
+++ db.go
@@ -14,8 +14,8 @@ const ( //Insert your own Credentials
 	username        = "root"
 	password        = "cogitoergosum"
 	hostname        = "127.0.0.1:3306"
-	dbname          = "newsa"
-	tableName       = "linksa"
+	dbname          = "newdb2"
+	tableName       = "newtable2"
 	column1         = "title"
 	column2         = "href"
 	maxIdleConns    = 20
@@ -158,7 +158,7 @@ func configureConnection(db *sql.DB) {
 }
 
 func openCreateTable(db *sql.DB, tablename string, column1 string, column2 string) {
-	query := fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s(%s text, %s text)", tablename, column1, column2)
+	query := fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s(id int primary key auto_increment, %s text, %s text)", tablename, column1, column2)
 	cntx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancelfunc()
 	res, err := db.ExecContext(cntx, query)
